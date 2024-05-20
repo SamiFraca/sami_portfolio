9b705738d259468a0a1e2048bde6c18a52acec56
SamiFraca <sami.fraca@flat101.es>
2024-05-20 14:08:51 +0200

    Add about page text and structure

---
+++ src/app/about/page.tsx
@@ -0,0 +1,37 @@
+export default function AboutPage() {
+  return (
+    <div className="flex flex-col gap-4">
+      <h1 className="text-4xl">About Me</h1>
+      <p>Hi everyone, I&apos;m Sami.</p>
+      <p>
+        Since I was young, I have always had a passion for computers and
+        technology, which guided me towards a career that I find comfortable and
+        fulfilling.
+      </p>
+      <p>
+        I went into a higher degree involving software web development and since
+        day one, i knew this was for me.
+      </p>
+      <p>
+        After that, i started working at Flat101 at 19 years old, (where i am
+        still).Now im 21 and i have been working on web development since then.
+      </p>
+      <p>
+        I focus on ecommerce development, involving databases,backend and
+        frontend.
+      </p>
+      <p>
+        In my free time, I enjoy exploring different technologies and approaches
+        to enhance my professional growth and skills.
+      </p>
+
+      <p>
+        I consider myself a technology enthusiast, I enjoy discovering new
+        things and learning from them. Whenever I have an opportunity to explore new ideas, I&apos;m always up
+        for the challenge
+      </p>
+     
+     
+    </div>
+  );
+}
