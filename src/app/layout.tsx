20511a28bcb9460b84d8897bd4eaaffac4ff5719
SamiFraca <sami.fraca@flat101.es>
2024-05-23 12:00:44 +0200

    feat: :sparkles: Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,6 @@ export const metadata: Metadata = {
   title: "Samidev",
   description: "Portfolio made by samidev",
 };
-
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -17,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="16x16" />
+      </head>
       <body
         className={`${inter.className} max-w-screen-md flex mx-auto md:mt-16 mt-8 justify-center items-center `}
       >
