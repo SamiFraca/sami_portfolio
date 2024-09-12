f3a4b319cc45daac31222d548ebad04c162e06cf
SamiFraca <fracasami@gmail.com>
2024-09-12 12:39:45 +0200

    Add new blog post and refactor hrefs

--- src/data/Posts.tsx
+++ src/data/Posts.tsx
@@ -2,12 +2,18 @@ import { BlogPostItemProps } from "@/components/BlogPostItem";
 
 export const Posts: BlogPostItemProps[] = [
   {
-    href: "https://samiblog.netlify.app/posts/2024-04-19/",
+    href: "https://samiblog.netlify.app/posts/997-2024-09-12/",
+    title: "8 Frontend Tools to Become a Better Developer",
+    description:
+      "Developer Resources for UI, Animation, and Web Development",
+  },
+  {
+    href: "https://samiblog.netlify.app/posts/998-2024-04-19/",
     title: "React 19 Enhanced hooks",
     description: "New react 19 hooks explained",
   },
   {
-    href: "https://samiblog.netlify.app/posts/introduction/",
+    href: "https://samiblog.netlify.app/posts/999-introduction/",
     title: "My First Blog Post",
     description:
       "Introduction to my blog",
